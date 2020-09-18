8a747ddfb8508665bb6576b87ef80b65988e0be8
luisabellan <luis.abellan.dev@gmail.com>
2020-09-18 11:25:24 +0200

    save

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
 <div id="app">
     <h1>Bitcoin Price Index</h1>
-    <div v-for=" c in c" v-bind:key="c.id" class="currency">
-        {{ c.description }}:
+    <div v-for=" curr in currencies" v-bind:key="curr.id" class="currencies">
+        {{ curr.description }}:
         <span class="lighten">
-            <span v-html="c.symbol"></span>{{ c.rate_float.toFixed(2) }}
+            <span v-html="curr.symbol"></span>{{ curr.rate_float.toFixed(2) }}
         </span>
     </div>
 </div>
@@ -15,12 +15,15 @@ import axios from 'axios'
 
 export default {
     name: 'App',
+    curr: [],
 
-    mounted() {
+    computed() {
+
         axios
             .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-            .then(response => {
-                this.c = response.data.bpi
+            .then(res => {
+                this.currencies = res.data.bpi
+                console.log(this.curr);
             })
             .catch(error => {
                 console.log(error)
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+Vue.useCssModule(VueAxios,axios)
